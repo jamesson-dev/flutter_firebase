ee37bd59c46d74a952e273f96992f1e9ffc640e3
Jamesson Ferreira <jamessonferreira69@gmail.com>
2022-06-27 01:04:42 -0300

    Login and Register page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Capyba App',
-      home: Dashboard(),
+      home: const Login(),
     );
   }
 }
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:hello_world/modules/dashboard/dashboard_page.dart';
+import 'package:hello_world/modules/signUp/signUp_page.dart';
+
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -10,11 +13,92 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Login")),
-       )
+     return Scaffold(
+      backgroundColor: Colors.white,
+      // appBar: AppBar(
+      //   title: Text("Login Page"),
+      // ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: SizedBox(
+                    width: 200,
+                    height: 150,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: Image.asset('assets/profile.png')),
+              ),
+            ),
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            TextButton(
+              onPressed: (){
+                //TODO FORGOT PASSWORD SCREEN GOES HERE
+              },
+              child: const Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    // or Navigator.pushReplacement
+                      context, MaterialPageRoute(builder: (_) => Dashboard()));
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+            TextButton(
+              onPressed: (){
+                 Navigator.push(
+                    // or Navigator.pushReplacement
+                      context, MaterialPageRoute(builder: (_) => const SignUp()));
+              },
+              child: const Text(
+                'New User? Create Account',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
